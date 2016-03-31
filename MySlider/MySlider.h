4bde08a828364282a943398c91aa36d1da7e2f8c
杜甲 <815319775@qq.com>
2016-03-31 16:24:46 +0800

    commit

---
+++ MySlider/MySlider.h
@@ -0,0 +1,16 @@
+//
+//  MySlider.h
+//  PodDemo1
+//
+//  Created by 杜甲 on 16/3/31.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MySlider : NSObject
+
+- (void) mydisplay;
+
+
+@end
---
+++ MySlider/MySlider.m
@@ -0,0 +1,18 @@
+//
+//  MySlider.m
+//  PodDemo1
+//
+//  Created by 杜甲 on 16/3/31.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import "MySlider.h"
+
+@implementation MySlider
+
+- (void)mydisplay
+{
+
+    NSLog(@"mydisplay");
+}
+@end
